8988d5044138f227bbe2ff68876696dcd6978ae3
OverPoweredDev <omkar.prabhune19@vit.edu>
2021-09-10 13:56:08 +0530

    works

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const days = ['#Sunday', '#Monday', '#Tuesday', '#Wednesday', '#Thursday', '#Friday', '#Saturday'];
-let day = days[new Date().getDay()];
-location.href = day;
+let today = days[new Date().getDay()];
+let tomorrow = days[(new Date().getDay() + 1)%7]
+
+$(today).css('display', 'block');
+$(today).nextUntil(tomorrow).css('display', 'block');
