b84f5c6a2819b941ac2c9eb6d519d293c3dca9e6
OverPoweredDev <omkar.prabhune19@vit.edu>
2021-09-14 11:01:27 +0530

    works

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ let tomorrow = days[(new Date().getDay() + 1)%7]
 
 $(today).css('display', 'block');
 $(today).nextUntil(tomorrow).css('display', 'block');
+
+let allDisplayed = false;
+$('.active').on('click', function () {
+    if(allDisplayed){
+        $('.list-group-item').css('display', 'none');
+        $(today).css('display', 'block');
+        $(today).nextUntil(tomorrow).css('display', 'block');
+        allDisplayed = false;
+    } else {
+        $('.list-group-item').css('display', 'block');
+        allDisplayed = true;
+    }
+});
